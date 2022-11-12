d3093c1546ae09245d2a412372793b1145425bb0
JeelRajodiya <jeelrajodiyajeel@gmail.com>
2022-11-12 18:11:29 +0530

    fix

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,9 +1,9 @@
 import Head from "next/head";
-export default function Layout({ children }) {
+export default function Layout({ children }: any) {
 	return (
 		<>
 			<Head>
-				<title>Jeel Rajodiya's Portfolio</title>
+				<title>Jeel Rajodiya&apos;s Portfolio</title>
 				<meta
 					name="description"
 					content="Jeel Rajodiya is a software developer focused in developing softwares on web technologies such as JavaScript and NextJS. He is open for Internship and Job opportunities in Ahmedabad, India and Surat, India."
