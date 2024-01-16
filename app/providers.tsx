9ce4357eccf96eff042739f2177c062bd877dcc8
JeelRajodiya <jeelrajodiyajeel@gmail.com>
2024-01-16 18:15:25 +0530

    Update Google Analytics package and remove Google Tag Manager

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,12 +1,14 @@
 // app/providers.tsx
 "use client";
-import { GoogleTagManager } from "@next/third-parties/google";
+import { GoogleAnalytics } from "nextjs-google-analytics";
+// import { GoogleTagManager } from "@next/third-parties/google";
 import { ChakraProvider } from "@chakra-ui/react";
 import theme from "@/styles/theme";
 export function Providers({ children }: { children: React.ReactNode }) {
 	return (
 		<ChakraProvider theme={theme}>
-			<GoogleTagManager gtmId="GTM-MM6BH258" />
+			<GoogleAnalytics />
+			{/* <GoogleTagManager gtmId="GTM-MM6BH258" /> */}
 			{children}
 		</ChakraProvider>
 	);
