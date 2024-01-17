2e71a81c3bb93f33dbc609178770d586a764c339
JeelRajodiya <jeelrajodiyajeel@gmail.com>
2024-01-17 11:33:38 +0530

    Update route.ts to increment visitors count and retrieve visitor data

--- app/api/views/route.ts
+++ app/api/views/route.ts
@@ -6,9 +6,43 @@ export async function POST(request: NextRequest) {
 	const db = client.db("portfolio");
 	const collection = db.collection("metadata");
 	const date = new Date();
-	await collection.updateOne;
+	await collection.updateOne(
+		{
+			type: "visitors",
+		},
+		{
+			$inc: {
+				visitors: 1,
+			},
+			$set: {
+				lastView: date,
+			},
+		},
+		{
+			upsert: true,
+		}
+	);
 
-	return new NextResponse(JSON.stringify({ message: "Feedback added" }), {
+	return new NextResponse(JSON.stringify({ message: "Done" }), {
+		status: 200,
+	});
+}
+
+export async function GET() {
+	const client = await clientPromise;
+	const db = client.db("portfolio");
+	const collection = db.collection("metadata");
+	const data = await collection.findOne(
+		{ type: "visitors" },
+		{
+			projection: {
+				_id: 0,
+				visitors: 1,
+			},
+		}
+	);
+	const visitors = data?.visitors || 0;
+	return new NextResponse(JSON.stringify({ visitors }), {
 		status: 200,
 	});
 }
